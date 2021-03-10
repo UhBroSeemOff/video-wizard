072f2f279ddbe9e644b2a5d73649ff1c50a88c3a
Abrosimov Vova <fantomklin@outlook.com>
2021-03-10 21:45:07 +0700

    I've added queue for request

--- Wizard_Manager.py
+++ Wizard_Manager.py
@@ -1,5 +1,6 @@
 ﻿from wizards.Storyboard_Wizard import Storyboard_Wizard
 from wizards.File_Wizard import File_Wizard
+from queue import Queue
 
 class Wizard_manager:
     def __init__(self):
@@ -8,6 +9,7 @@ class Wizard_manager:
         self.__files = []
         self.__selected_file = ""
         self.__extension = ""
+        self.__request_queue = Queue()
         self.__command_map = {
             "help": self.__help__,
             "look_for": self.__look_for__,
@@ -18,31 +20,41 @@ class Wizard_manager:
             "storyboard": self.__cast_storyboard_spell__
         }
 
-    def make_wise_request(self, request):
-        if request in self.__command_map.keys():
-            self.__command_map[request]()
-        else:
-            print("Try another spell \n")
+    def make_wise_request(self, input_queue):
+        self.__store_queue__(input_queue)
+        if self.__request_queue.empty():
+            return
+        self.__process_queue__()
 
     def __help__(self):
         [ print("   {}".format(command)) for command in self.__command_map.keys() if command != "help"]
 
     def __look_for__(self):
-        self.__extension = input("Specify file extension you are looking for\n")
+        if self.__request_queue.empty() == True:
+            self.__extension = input("Specify file extension you are looking for\n")
+        else:
+            self.__extension = self.__request_queue.get()
         self.__files = self.__file_wizard.look_around(self.__extension)
         print("File_Wizard: I found {} files!".format(len(self.__files)))
 
     def __select__(self):
-        print("Choose file's index")
-        self.__look_around__()
-        number_of_selection = int(input())
+        if self.__request_queue.empty() == True:
+            print("Choose file's index")
+            self.__look_around__()
+            number_of_selection = int(input())
+        else:
+            parameter = self.__request_queue.get()
+            number_of_selection = self.__convert_parameter_to_file_index__(parameter)
         if number_of_selection > 0 & number_of_selection <= len(self.__files):
             self.__selected_file = self.__files[number_of_selection-1]
         else:
             print("Wrong index\n")
 
     def __cast__(self):
-        spell = input("Choose your spell\n")
+        if self.__request_queue.empty() == True:
+            spell = input("Choose your spell\n")
+        else:
+            spell = self.__request_queue.get()
         if spell in self.__spell_map:
             self.__spell_map[spell]()
         else:
@@ -61,9 +73,31 @@ class Wizard_manager:
                 print(" {} {}".format(str(file_number), str(file)))
                 file_number += 1
 
+    def __store_queue__(self, queue):
+        while queue.empty() == False:
+            self.__request_queue.put(queue.get())
+
+    def __process_queue__(self):
+        while self.__request_queue.empty() == False:
+            command = self.__request_queue.get()
+            if command in self.__command_map.keys():
+                self.__command_map[command]()
+            else:
+                print("Try another spell \n")
+                self.__request_queue = Queue()
+
+    def __convert_parameter_to_file_index__(self, parameter):
+        if parameter == "last":
+            return len(self.__files)
+        if parameter == "first":
+            return 1
+        return int(parameter)
+
 if __name__ == "__main__":
     manager = Wizard_manager()  
     print("Type 'help' for commands list\n") 
     while True:
-        command = input()
-        manager.make_wise_request(command)
+        input_array = input().split(' ')
+        input_queue = Queue()
+        [input_queue.put(element) for element in input_array]
+        manager.make_wise_request(input_queue)
